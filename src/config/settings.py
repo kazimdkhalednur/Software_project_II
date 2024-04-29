a007a796af3227c5a740ad9f6bbdfd5ccad29fea
Kazi Khaled Nur <kazikhalednur@gmail.com>
2024-04-29 12:47:09 +0600

    static file configuratin added

--- src/config/settings.py
+++ src/config/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
+STATIC_ROOT = BASE_DIR / "static"
+MEDIA_URL = "uploads/"
+MEDIA_ROOT = BASE_DIR / "uploads"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

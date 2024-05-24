3df558aaf7dbd15a34a21941f13eae889dee2cfa
Kazi Khaled Nur <kazikhalednur@gmail.com>
2024-05-24 16:05:19 +0600

    update prooduct admin

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,21 @@ from django.contrib import admin
 
 from .models import Cart, Category, Product, ProductImage
 
-admin.site.register(Product)
 admin.site.register(Category)
-admin.site.register(ProductImage)
 admin.site.register(Cart)
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 5
+    max_num = 5
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]
+    list_filter = ["category", "status"]
+    list_display = ["title", "discount_price", "price", "quantity"]
+
+    class Meta:
+        model = Product

b3b2391cc68ac50868c3d5292180928964b9e5f7
Kazi Khaled Nur <kazikhalednur@gmail.com>
2024-05-24 16:01:09 +0600

    update product list url

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "products"
 
 urlpatterns = [
-    path("", views.ProductListView.as_view(), name="product_list"),
+    path("products/", views.ProductListView.as_view(), name="product_list"),
     path("cart/", views.CartListView.as_view(), name="cart_list"),
     path("cart/create/", views.CartCreateView.as_view(), name="cart_create"),
     path("<str:slug>/", views.ProductDetailView.as_view(), name="product_detail"),
